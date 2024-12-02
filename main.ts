3a56407a38fdb40df612583bd28267942cf88e41
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2024-12-02 08:26:15 +0100

    Removed hard-coded channel ID

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ import { BlueskyDiscordBot } from "./discord/client.ts";
 
 const bot = new BlueskyDiscordBot({
 	bskyService: "https://public.api.bsky.app/xrpc",
-	channelId: "660752972858392589",
 	// biome-ignore lint/style/noNonNullAssertion: <explanation>
 	discordToken: Deno.env.get("DISCORD_TOKEN")!,
 	trackedUser: "auonsson.bsky.social",
