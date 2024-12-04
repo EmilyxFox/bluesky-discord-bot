dfd88df40678f7d7f3592862a9e4398fd8e75bf1
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2024-12-04 20:23:48 +0100

    remove tracked user and add token validation

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 import { BlueskyDiscordBot } from "./discord/client.ts";
 
+const token = Deno.env.get("DISCORD_TOKEN");
+
+if (!token) {
+	throw new Error('No discord token provided. Please set ENV "DISCORD_TOKEN"');
+}
 const bot = new BlueskyDiscordBot({
 	bskyService: "https://public.api.bsky.app/xrpc",
 	// biome-ignore lint/style/noNonNullAssertion: <explanation>
 	discordToken: Deno.env.get("DISCORD_TOKEN")!,
-	trackedUser: "auonsson.bsky.social",
 });
 
 bot.initialise();
