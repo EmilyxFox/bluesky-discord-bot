69259938a1d0a3b0b52812e983a4f6851edaa995
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2024-12-05 12:31:42 +0100

    removed old commands

--- discord/commandHandler.ts
+++ discord/commandHandler.ts
@@ -1,6 +1,5 @@
 import type { Command } from "$types/command.ts";
 import { type ChatInputCommandInteraction, REST, Routes } from "discord.js";
-import { TrackCommand } from "./commands/track.ts";
 import type { BlueskyDiscordBot } from "./client.ts";
 import { SubscriptionsCommand } from "./commands/subscriptions.ts";
 import { TestEmbedCommand } from "./commands/testEmbed.ts";
@@ -14,11 +13,7 @@ export class CommandHandler {
 		if (!token)
 			throw new Error("Invalid Discord token when registering commands");
 
-		this.commands = [
-			new TrackCommand(),
-			new SubscriptionsCommand(),
-			new TestEmbedCommand(),
-		];
+		this.commands = [new SubscriptionsCommand(), new TestEmbedCommand()];
 		this.discordREST = new REST().setToken(token);
 
 		const clientId = Deno.env.get("CLIENT_ID");
