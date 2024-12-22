1135f04304128a55a2dc634d4a2375af1a585d69
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2024-12-22 14:42:07 +0100

    added env variable to change bluesky api

--- main.ts
+++ main.ts
@@ -2,11 +2,13 @@ import { BlueskyDiscordBot } from "./discord/client.ts";
 
 const token = Deno.env.get("DISCORD_TOKEN");
 
+const api = Deno.env.get("SERVICE_API") || "https://public.api.bsky.app/xrpc";
+
 if (!token) {
 	throw new Error('No discord token provided. Please set ENV "DISCORD_TOKEN"');
 }
 const bot = new BlueskyDiscordBot({
-	bskyService: "https://public.api.bsky.app/xrpc",
+	bskyService: api,
 	discordToken: token,
 });
 
