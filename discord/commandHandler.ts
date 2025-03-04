2dec0349e59e1d77708e363c8d491ed9de33b65e
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com>
2025-03-04 22:50:15 +0100

    ✏️ Fix typo in command execution log message.

--- discord/commandHandler.ts
+++ discord/commandHandler.ts
@@ -64,7 +64,7 @@ export class CommandHandler {
       .run(interaction, botClient)
       .then(() => {
         console.log(
-          `Sucesfully executed command [/${interaction.commandName} ${interaction.options.getSubcommand()}]`,
+          `Successfully executed command [/${interaction.commandName} ${interaction.options.getSubcommand()}]`,
           {
             guild: { id: interaction.guildId },
             user: { name: interaction.user.globalName },
